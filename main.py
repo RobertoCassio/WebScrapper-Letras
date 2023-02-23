cc98a1b4e9826f7713280f839a5e33ab5098c681
RobertoCassio <rcslino@gmail.com>
2023-02-23 12:41:34 -0300

    second commit

--- main.py
+++ main.py
@@ -7,25 +7,30 @@ options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 
 drive = webdriver.Chrome(options=options)
-music = input('Digite a música:')
+while True:
+    music = input('Digite a música:').strip()
+    if not music:
+        print("Nome da música não informado")
+    else:
 
-drive.get('https://www.letras.mus.br/')
+        drive.get('https://www.letras.mus.br/')
 
-elem = drive.find_element(By.ID, 'main_suggest')
-elem.clear()
-elem.send_keys(music)
-elem.send_keys(Keys.ENTER)
-print("Busca feita com sucesso")
+        elem = drive.find_element(By.ID, 'main_suggest')
+        elem.clear()
+        elem.send_keys(music)
+        elem.send_keys(Keys.ENTER)
+        print("Busca feita com sucesso")
 
-elem = drive.find_element(By.XPATH,
-                          '/html/body/div[1]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div/div[5]/div['
-                          '2]/div/div/div[1]/div[1]/div[1]/div[1]/div/a')
-elem.click()
-print("Página encontra com sucesso")
+        elem = drive.find_element(By.XPATH,
+                                  '/html/body/div[1]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div/div[5]/div['
+                                  '2]/div/div/div[1]/div[1]/div[1]/div[1]/div/a')
+        elem.click()
+        print("Página encontra com sucesso")
 
-try:
-    letra = drive.find_element(By.CLASS_NAME, 'cnt-letra').text
-except NoSuchElementException:
-    letra = drive.find_element(By.CLASS_NAME, 'cnt-trad_l ').text
+        try:
+            letra = drive.find_element(By.CLASS_NAME, 'cnt-letra').text
+        except NoSuchElementException:
+            letra = drive.find_element(By.CLASS_NAME, 'cnt-trad_l ').text
 
-print(f'Segue a letra: \n{letra}')
+        print(f'Segue a letra: \n{letra}')
+        break
